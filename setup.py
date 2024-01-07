4c5c93d4a1116af7a8604d13ab003313b391a90d
Dapunta <76211798+Dapunta@users.noreply.github.com>
2024-01-07 21:59:22 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.md','r',encoding = 'utf-8') as doc:
 setup(
   name = 'FBTools',
   packages = ['FBTools'],
-  version = '0.0.1',
+  version = '0.0.2',
   license='MIT',
   description = 'Facebook Scrapper',
   long_description=readme,
